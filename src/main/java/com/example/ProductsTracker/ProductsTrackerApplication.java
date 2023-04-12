04d0c537695c2e3689a75c40994f990fdfb2b411
MattCza <mtuczajkowski@gmail.com>
2023-04-12 20:31:08 +0200

    generated Spring project

---
+++ src/main/java/com/example/ProductsTracker/ProductsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProductsTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProductsTracker/ProductsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductsTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
