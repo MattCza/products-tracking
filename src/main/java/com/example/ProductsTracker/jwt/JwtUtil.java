3aa0b71140c06a3aa2b218e68786ba453f0a928a
MattCza <mtuczajkowski@gmail.com>
2023-04-26 20:40:35 +0200

    start jwt

---
+++ src/main/java/com/example/ProductsTracker/jwt/JwtUtil.java
@@ -0,0 +1,10 @@
+package com.example.ProductsTracker.jwt;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUtil {
+    public String getSubject(String token) {
+        return null;
+    }
+}
---
+++ src/main/java/com/example/ProductsTracker/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.ProductsTracker.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
